cd37a05d63f7f0a0d80fffcdc17830539dc45f2b
Michael Bestas <mkbestas@lineageos.org>
2024-07-22 00:03:18 +0100

    duchamp: overlay: Enable battery cycle count
    Change-Id: I92bc8363e1c9e2ad54c29a0a30dce992e25f697d

--- device.mk
+++ device.mk
@@ -184,7 +184,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
    LineageApertureOverlayDuchamp \
-   LineageDialerDuchamp
+   LineageDialerDuchamp \
+   LineageSettingsOverlayDuchamp
 
 # Parts
 PRODUCT_PACKAGES += \
