02dafe5396b138489eae9e844780b43c026cd4b7
Ramii Ahmed <ramy@ahmedramy.com>
2025-01-09 18:00:22 +0800

    duchamp: parts: Initialize High Touch Polling Rate support
    Used to handle our node to force enable Xiaomi's High Touch polling rate mode from kernel
    
    Adapted High Touch Polling Rate support to S style
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -39,6 +39,7 @@ import org.lineageos.settings.doze.PocketService;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.thermal.ThermalTileService;
 import org.lineageos.settings.refreshrate.RefreshUtils;
+import org.lineageos.settings.touchsampling.TouchSamplingUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final String TAG = "XiaomiParts";
@@ -95,6 +96,9 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         // Start Pocket Mode Service
         PocketService.startService(context);
+
+        // Start HTSR
+        TouchSamplingUtils.restoreSamplingValue(context);
     }
 
     private void overrideHdrTypes(Context context) {
