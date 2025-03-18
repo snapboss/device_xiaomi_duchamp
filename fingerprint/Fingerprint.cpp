cf303ffe2bb1f945b20ef9ffe77fd23efbbb7022
rio004 <riyon336@gmail.com>
2025-03-18 22:08:37 +0000

    duchamp: fingerprint: Implement Xiaomi fingerprint functions
    * fingerprint-xiaomi.h:
        Based on <hardware/fingerprint.h> and hardware/xiaomi/fingerprint/include/fingerprint.h
        Reversed odm/lib64/hw/fingerprint.goodix_fod.so of OS2.0.101.0.VNBCNXM
        Also merged udfpshandler
    
    * Legacy2Aidl.h:
        Import from hardware/xiaomi/aidl/fingerprint/include/Legacy2Aidl.h
    
    * Some changes based on hardware/xiaomi/aidl/fingerprint
    
    Based on by my initial aidl changes houji-development/android_hardware_xiaomi-old@08b15ec
    
    As goodix_fod on VNDK34 is no longer store enrollment id on gid, but rather as fid
    
    Co-Authored-by: kmiit <kmiit@foxmail.com>
    Co-Authored-by: lolipuru <banstellazz@gmail.com>
    Change-Id: I342c971bbefd03078e841ab45d16c9d13526011b
    Signed-off-by: rio004 <riyon336@gmail.com>

--- fingerprint/Fingerprint.cpp
+++ fingerprint/Fingerprint.cpp
@@ -5,17 +5,109 @@
  */
 
 #include "Fingerprint.h"
+#include "Session.h"
+
+#include <android-base/properties.h>
+#include <fingerprint.sysprop.h>
+
+#include <android-base/logging.h>
+#include <android-base/strings.h>
 
 namespace aidl::android::hardware::biometrics::fingerprint {
 
-ndk::ScopedAStatus Fingerprint::getSensorProps(std::vector<SensorProps>* /*out*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+namespace {
+constexpr size_t MAX_WORKER_QUEUE_SIZE = 5;
+constexpr int MAX_ENROLLMENTS_PER_USER = 5;
+constexpr bool SUPPORTS_NAVIGATION_GESTURES = true;
+constexpr char HW_COMPONENT_ID[] = "fingerprintSensor";
+constexpr char HW_VERSION[] = "vendor/model/revision";
+constexpr char FW_VERSION[] = "1.01";
+constexpr char SERIAL_NUMBER[] = "00000001";
+constexpr char SW_COMPONENT_ID[] = "matchingAlgorithm";
+constexpr char SW_VERSION[] = "vendor/version/revision";
+
+}  // namespace
+
+static Fingerprint* sInstance;
+
+Fingerprint::Fingerprint() : mWorker(MAX_WORKER_QUEUE_SIZE) {
+    sInstance = this;  // keep track of the most recent instance
+
+    std::string sensorTypeProp = Fingerprint::cfg().get<std::string>("type");
+    if (sensorTypeProp == "udfps" || sensorTypeProp == "udfps_optical") {
+        if (sensorTypeProp == "udfps") {
+            mSensorType = FingerprintSensorType::UNDER_DISPLAY_ULTRASONIC;
+        } else {
+            mSensorType = FingerprintSensorType::UNDER_DISPLAY_OPTICAL;
+        }
+    } else if (sensorTypeProp == "side") {
+        mSensorType = FingerprintSensorType::POWER_BUTTON;
+    } else if (sensorTypeProp == "home") {
+        mSensorType = FingerprintSensorType::HOME_BUTTON;
+    } else if (sensorTypeProp == "rear") {
+        mSensorType = FingerprintSensorType::REAR;
+    } else {
+        mSensorType = FingerprintSensorType::UNKNOWN;
+        UNIMPLEMENTED(FATAL) << "unrecognized or unimplemented fingerprint behavior: "
+                             << sensorTypeProp;
+    }
+
+    mEngine = std::make_unique<FingerprintEngine>();
+    LOG(INFO) << "sensorTypeProp:" << sensorTypeProp;
 }
 
-ndk::ScopedAStatus Fingerprint::createSession(int32_t /*sensorId*/, int32_t /*userId*/,
-                                              const std::shared_ptr<ISessionCallback>& /*cb*/,
-                                              std::shared_ptr<ISession>* /*out*/) {
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+void Fingerprint::notify(const fingerprint_msg_t* msg) {
+    Fingerprint* thisPtr = sInstance;
+    if (thisPtr == nullptr || thisPtr->mSession == nullptr || thisPtr->mSession->isClosed()) {
+        LOG(ERROR) << "Receiving callbacks before a session is opened.";
+        return;
+    }
+    thisPtr->mSession->notify(msg);
+}
+
+ndk::ScopedAStatus Fingerprint::getSensorProps(std::vector<SensorProps>* out) {
+    std::vector<common::ComponentInfo> componentInfo = {
+            {HW_COMPONENT_ID, HW_VERSION, FW_VERSION, SERIAL_NUMBER, "" /* softwareVersion */},
+            {SW_COMPONENT_ID, "" /* hardwareVersion */, "" /* firmwareVersion */,
+             "" /* serialNumber */, SW_VERSION}};
+    auto sensorId = Fingerprint::cfg().get<std::int32_t>("sensor_id");
+    auto sensorStrength = Fingerprint::cfg().get<std::int32_t>("sensor_strength");
+    auto navigationGuesture = Fingerprint::cfg().get<bool>("navigation_guesture");
+    auto detectInteraction = Fingerprint::cfg().get<bool>("detect_interaction");
+    auto displayTouch = Fingerprint::cfg().get<bool>("display_touch");
+    auto controlIllumination = Fingerprint::cfg().get<bool>("control_illumination");
+
+    common::CommonProps commonProps = {sensorId, (common::SensorStrength)sensorStrength,
+                                       MAX_ENROLLMENTS_PER_USER, componentInfo};
+
+    SensorLocation sensorLocation = mEngine->getSensorLocation();
+
+    LOG(INFO) << "sensor type:" << ::android::internal::ToString(mSensorType)
+              << " location:" << sensorLocation.toString();
+
+    *out = {{commonProps,
+             mSensorType,
+             {sensorLocation},
+             navigationGuesture,
+             detectInteraction,
+             displayTouch,
+             controlIllumination,
+             std::nullopt}};
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus Fingerprint::createSession(int32_t sensorId, int32_t userId,
+                                              const std::shared_ptr<ISessionCallback>& cb,
+                                              std::shared_ptr<ISession>* out) {
+    CHECK(mSession == nullptr || mSession->isClosed()) << "Open session already exists!";
+
+    mSession = SharedRefBase::make<Session>(sensorId, userId, cb, mEngine.get(), &mWorker);
+    *out = mSession;
+
+    mSession->linkToDeath(cb->asBinder().get());
+
+    LOG(INFO) << __func__ << ": sensorId:" << sensorId << " userId:" << userId;
+    return ndk::ScopedAStatus::ok();
 }
 
 }  // namespace aidl::android::hardware::biometrics::fingerprint
--- fingerprint/Session.h
+++ fingerprint/Session.h
@@ -5,47 +5,72 @@
  */
 
 #pragma once
+#define LOG_TAG "android.hardware.biometrics.fingerprint-service.xiaomi"
 
 #include <aidl/android/hardware/biometrics/fingerprint/BnSession.h>
+#include <aidl/android/hardware/biometrics/fingerprint/ISessionCallback.h>
+#include "thread/WorkerThread.h"
 
-using ::aidl::android::hardware::biometrics::common::ICancellationSignal;
-using ::aidl::android::hardware::biometrics::common::OperationContext;
-using ::aidl::android::hardware::biometrics::fingerprint::PointerContext;
-using ::aidl::android::hardware::keymaster::HardwareAuthToken;
+#include "FingerprintEngine.h"
+#include "Legacy2Aidl.h"
+
+namespace common = aidl::android::hardware::biometrics::common;
+namespace keymaster = aidl::android::hardware::keymaster;
 
 namespace aidl::android::hardware::biometrics::fingerprint {
 
+void onClientDeath(void* cookie);
+
 class Session : public BnSession {
   public:
+    Session(int sensorId, int userId, std::shared_ptr<ISessionCallback> cb,
+            FingerprintEngine* engine, WorkerThread* worker);
+
     ndk::ScopedAStatus generateChallenge() override;
     ndk::ScopedAStatus revokeChallenge(int64_t challenge) override;
-    ndk::ScopedAStatus enroll(const HardwareAuthToken& hat,
-                              std::shared_ptr<ICancellationSignal>* out) override;
+    ndk::ScopedAStatus enroll(const keymaster::HardwareAuthToken& hat,
+                              std::shared_ptr<common::ICancellationSignal>* out) override;
     ndk::ScopedAStatus authenticate(int64_t operationId,
-                                    std::shared_ptr<ICancellationSignal>* out) override;
-    ndk::ScopedAStatus detectInteraction(std::shared_ptr<ICancellationSignal>* out) override;
+                                    std::shared_ptr<common::ICancellationSignal>* out) override;
+    ndk::ScopedAStatus detectInteraction(
+            std::shared_ptr<common::ICancellationSignal>* out) override;
     ndk::ScopedAStatus enumerateEnrollments() override;
     ndk::ScopedAStatus removeEnrollments(const std::vector<int32_t>& enrollmentIds) override;
     ndk::ScopedAStatus getAuthenticatorId() override;
     ndk::ScopedAStatus invalidateAuthenticatorId() override;
-    ndk::ScopedAStatus resetLockout(const HardwareAuthToken& hat) override;
+    ndk::ScopedAStatus resetLockout(const keymaster::HardwareAuthToken& hat) override;
     ndk::ScopedAStatus close() override;
     ndk::ScopedAStatus onPointerDown(int32_t pointerId, int32_t x, int32_t y, float minor,
                                      float major) override;
     ndk::ScopedAStatus onPointerUp(int32_t pointerId) override;
     ndk::ScopedAStatus onUiReady() override;
-    ndk::ScopedAStatus authenticateWithContext(int64_t operationId, const OperationContext& context,
-                                               std::shared_ptr<ICancellationSignal>* out) override;
-    ndk::ScopedAStatus enrollWithContext(const HardwareAuthToken& hat,
-                                         const OperationContext& context,
-                                         std::shared_ptr<ICancellationSignal>* out) override;
+    ndk::ScopedAStatus authenticateWithContext(
+            int64_t operationId, const common::OperationContext& context,
+            std::shared_ptr<common::ICancellationSignal>* out) override;
+    ndk::ScopedAStatus enrollWithContext(
+            const keymaster::HardwareAuthToken& hat, const common::OperationContext& context,
+            std::shared_ptr<common::ICancellationSignal>* out) override;
     ndk::ScopedAStatus detectInteractionWithContext(
-            const OperationContext& context, std::shared_ptr<ICancellationSignal>* out) override;
+            const common::OperationContext& context,
+            std::shared_ptr<common::ICancellationSignal>* out) override;
     ndk::ScopedAStatus onPointerDownWithContext(const PointerContext& context) override;
     ndk::ScopedAStatus onPointerUpWithContext(const PointerContext& context) override;
-    ndk::ScopedAStatus onContextChanged(const OperationContext& context) override;
+    ndk::ScopedAStatus onContextChanged(const common::OperationContext& context) override;
     ndk::ScopedAStatus onPointerCancelWithContext(const PointerContext& context) override;
     ndk::ScopedAStatus setIgnoreDisplayTouches(bool shouldIgnore) override;
+
+    void notify(const fingerprint_msg_t* msg);
+    bool isClosed();
+    binder_status_t linkToDeath(AIBinder* binder);
+
+  private:
+    int mSensorId;
+    int mUserId;
+    std::shared_ptr<ISessionCallback> mCb;
+    FingerprintEngine* mEngine;
+    WorkerThread* mWorker;
+    AIBinder_DeathRecipient* mDeathRecipient;
+    bool mIsClosed = false;
 };
 
 }  // namespace aidl::android::hardware::biometrics::fingerprint
