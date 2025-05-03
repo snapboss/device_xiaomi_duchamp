113a9d0cc34f04816dab72b0cc88679b20fb676f
snapboss <mbajrami95@gmail.com>
2025-05-03 06:20:38 +0200

    duchamp: parts: Drop RefreshRate QS Tile
    Our display currently goes black if set to 30hz. Removing tile to avoid that. For now.

--- parts/src/org/lineageos/settings/TileEntryActivity.java
+++ parts/src/org/lineageos/settings/TileEntryActivity.java
@@ -26,13 +26,11 @@ import android.util.Log;
 
 import androidx.annotation.Nullable;
 
-import org.lineageos.settings.refreshrate.RefreshActivity;
 import org.lineageos.settings.thermal.ThermalSettingsActivity;
 import org.lineageos.settings.saturation.SaturationActivity;
 
 public class TileEntryActivity extends Activity {
     private static final String TAG = "TileEntryActivity";
-    private static final String REFRESH_TILE = "org.lineageos.settings.refreshrate.RefreshTileService";
     private static final String THERMAL_TILE = "org.lineageos.settings.thermal.ThermalTileService";
     private static final String SATURATION_TILE = "org.lineageos.settings.saturation.SaturationTileService";
 
@@ -49,9 +47,7 @@ public class TileEntryActivity extends Activity {
         String sourceClassName = sourceComponent.getClassName();
         Intent intent = null;
 
-        if (REFRESH_TILE.equals(sourceClassName)) {
-            intent = new Intent(this, RefreshActivity.class);
-        } else if (THERMAL_TILE.equals(sourceClassName)) {
+        if (THERMAL_TILE.equals(sourceClassName)) {
             intent = new Intent(this, ThermalSettingsActivity.class);
         } else if (SATURATION_TILE.equals(sourceClassName)) {
             intent = new Intent(this, SaturationActivity.class);
