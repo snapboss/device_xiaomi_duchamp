cd09d7cb240141074cd59fedff9d7ef701864630
snapboss <mbajrami95@gmail.com>
2025-08-26 14:52:22 +0000

    duchamp: LineageOS common bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/framework_compatibility_matrix.xml \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
@@ -113,7 +113,7 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := erofs
 BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := erofs
 
 BOARD_PRODUCTIMAGE_MINIMAL_PARTITION_RESERVED_SIZE := false
--include vendor/aosp/config/BoardConfigReservedSize.mk
+-include vendor/lineage/config/BoardConfigReservedSize.mk
 
 TARGET_COPY_OUT_ODM := odm
 TARGET_COPY_OUT_ODM_DLKM := odm_dlkm
@@ -148,7 +148,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-BOOT_SECURITY_PATCH := 2025-01-01
+BOOT_SECURITY_PATCH := 2025-06-01
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SELinux
---
+++ lineage_duchamp.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/xiaomi/duchamp/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_duchamp
+PRODUCT_DEVICE := duchamp
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := 2311DRK48G
+PRODUCT_SYSTEM_NAME := duchamp_global
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="duchamp_global-user 15 AP3A.240905.015.A2 OS2.0.200.0.VNLMIXM release-keys" \
+    BuildFingerprint=POCO/duchamp_global/duchamp:14/UP1A.230905.011/OS2.0.200.0.VNLMIXM:user/release-keys \
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
+
