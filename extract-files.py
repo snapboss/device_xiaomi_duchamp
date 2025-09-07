fd97104be795c75bc07e971f0f97406538884cd6
rio004 <riyon336@gmail.com>
2025-09-07 12:53:14 +0000

    duchamp: Drop dolby stack

--- extract-files.py
+++ extract-files.py
@@ -50,9 +50,6 @@ blob_fixups: blob_fixups_user_type = {
     'odm/bin/hw/vendor.xiaomi.sensor.citsensorservice.aidl': blob_fixup()
         .add_needed('libui_shim.so'),
 
-    'vendor/lib64/c2.dolby.client.so': blob_fixup()
-        .add_needed('libshim_codec2_hidl.so'),
-
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
         .replace_needed('android.hardware.audio.common-V1-ndk.so', 'android.hardware.audio.common-V2-ndk.so')
         .replace_needed('libalsautils.so', 'libalsautils-v34.so'),
@@ -111,6 +108,10 @@ blob_fixups: blob_fixups_user_type = {
 
     'vendor/lib64/mt6897/libmtkcam_hwnode.jpegnode.so': blob_fixup()
         .add_needed('libultrahdr_shim.so'),
+
+    'vendor/etc/vintf/manifest/manifest_media_c2_V1_2_default.xml': blob_fixup()
+        .regex_replace('.+dolby.+\n', '')
+
 }  # fmt: skip
 
 module = ExtractUtilsModule(
