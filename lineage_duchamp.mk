8cce3f3038377b82492e3338e742f2b1afc35c3a
snapboss <mbajrami95@gmail.com>
2025-11-10 03:57:16 +0100

    duchamp: Update blobs to OS2.0.206.0.VNLMIXM

--- lineage_duchamp.mk
+++ lineage_duchamp.mk
@@ -29,6 +29,6 @@ PRODUCT_SYSTEM_NAME := duchamp_global
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=POCO/duchamp_global/duchamp:14/UP1A.230905.011/OS2.0.204.0.VNLMIXM:user/release-keys \
+    BuildFingerprint=POCO/duchamp_global/duchamp:14/UP1A.230905.011/OS2.0.206.0.VNLMIXM:user/release-keys \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
 
